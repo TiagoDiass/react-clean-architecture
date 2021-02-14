43e08713a164985dcec00fa318dc7fa1ebe7c56c
TiagoDiass <tiago.costadiasss@gmail.com>
2021-02-14 17:24:32 -0300

    refactor: moving logo to its own component

---
+++ src/presentation/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ImageLogo from '@/presentation/assets/logo.svg';
+
+const Logo: React.FC = () => {
+  return <img src={ImageLogo} alt='4Dev Enquetes para Programadores' />;
+};
+
+export default Logo;
--- src/presentation/pages/Login/Login.tsx
+++ src/presentation/pages/Login/Login.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import Styles from './Login.styles.scss';
-import Logo from '@/presentation/assets/logo.svg';
-import { Spinner } from '@/presentation/components';
+import { Spinner, Logo } from '@/presentation/components';
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
       <header className={Styles.header}>
-        <img src={Logo} alt='4Dev Enquetes para Programadores' />
+        <Logo />
         <h1>4Dev Enquetes para Programadores</h1>
       </header>
 
