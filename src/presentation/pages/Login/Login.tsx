e163182bf9d79980ca31a27fd30b45c242c05d2f
TiagoDiass <tiago.costadiasss@gmail.com>
2021-02-14 13:56:06 -0300

    feat: add login view

--- src/presentation/pages/Login/Login.tsx
+++ src/presentation/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Styles from './Login.styles.scss';
 import Logo from '@/presentation/assets/logo.svg';
+import { Spinner } from '@/presentation/components';
 
 const Login: React.FC = () => {
   return (
@@ -9,10 +10,32 @@ const Login: React.FC = () => {
         <img src={Logo} alt='4Dev Enquetes para Programadores' />
         <h1>4Dev Enquetes para Programadores</h1>
       </header>
-      <form className={Styles.form}></form>
-      <footer className={Styles.footer}>
-        <h2>Footer</h2>
-      </footer>
+
+      <form className={Styles.form}>
+        <h2>Login</h2>
+
+        <div className={Styles.inputWrapper}>
+          <input type='email' name='email' placeholder='Digite seu e-mail' />
+          <span className={Styles.status}>🔴</span>
+        </div>
+
+        <div className={Styles.inputWrapper}>
+          <input type='password' name='password' placeholder='Digite sua senha' />
+
+          <span className={Styles.status}>🔴</span>
+        </div>
+
+        <button type='submit'>Entrar</button>
+
+        <span className={Styles.link}>Quero me cadastrar</span>
+
+        <div className={Styles.errorWrapper}>
+          <Spinner className={Styles.spinner} />
+          <span className={Styles.error}>Erro</span>
+        </div>
+      </form>
+
+      <footer className={Styles.footer} />
     </div>
   );
 };
