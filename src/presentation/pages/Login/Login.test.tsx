e98d3030d79f366bf8d33be6d1cd49b4c5022717
TiagoDiass <tiago.costadiasss@gmail.com>
2021-02-15 22:34:04 -0300

    refactor: create a Sut factory to Login tests

--- src/presentation/pages/Login/Login.test.tsx
+++ src/presentation/pages/Login/Login.test.tsx
@@ -1,36 +1,48 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import Login from './Login';
+
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+
+  return {
+    sut,
+  };
+};
 
 describe('Login Component', () => {
   it('should not render Spinner and error on start', () => {
-    const { getByTestId } = render(<Login />);
-    const errorWrapper = getByTestId('error-wrapper');
+    const { sut } = makeSut();
+    const errorWrapper = sut.getByTestId('error-wrapper');
     expect(errorWrapper.childElementCount).toBe(0);
   });
 
   it('should disable the submit button on Login start because fields are not valid yet', () => {
-    const { getByTestId } = render(<Login />);
-    const submitButton = getByTestId('submit') as HTMLButtonElement;
+    const { sut } = makeSut();
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
   });
 
   it('should start with empty email and password inputs', () => {
-    const { getByTestId } = render(<Login />);
-    const emailInput = getByTestId('email-input') as HTMLInputElement;
-    const passwordInput = getByTestId('password-input') as HTMLInputElement;
+    const { sut } = makeSut();
+    const emailInput = sut.getByTestId('email-input') as HTMLInputElement;
+    const passwordInput = sut.getByTestId('password-input') as HTMLInputElement;
 
     expect(emailInput.value).toBe('');
     expect(passwordInput.value).toBe('');
   });
 
   it('should start email and passoword inputs with an "invalid" state', () => {
-    const { getByTestId } = render(<Login />);
-    const emailStatus = getByTestId('email-status');
+    const { sut } = makeSut();
+    const emailStatus = sut.getByTestId('email-status');
     expect(emailStatus.title).toBe('Campo obrigatório');
     expect(emailStatus.textContent).toBe('🔴');
 
-    const passwordStatus = getByTestId('password-status');
+    const passwordStatus = sut.getByTestId('password-status');
     expect(passwordStatus.title).toBe('Campo obrigatório');
     expect(passwordStatus.textContent).toBe('🔴');
   });
