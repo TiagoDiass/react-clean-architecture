7ee72a33d7d9177cda67652bbb4e490ed811ec0d
TiagoDiass <tiago.costadiasss@gmail.com>
2021-02-19 22:21:52 -0300

    refactor: changing sut factory to create a new EmailValidation with a random column name

--- src/validation/validators/email/email-validation.test.ts
+++ src/validation/validators/email/email-validation.test.ts
@@ -2,7 +2,7 @@ import { EmailValidation } from './email-validation';
 import { InvalidFieldError } from '@/validation/errors';
 import faker from 'faker';
 
-const makeSut = () => new EmailValidation('email');
+const makeSut = () => new EmailValidation(faker.database.column());
 
 describe('EmailValidation', () => {
   it('should return an error if email is invalid', () => {
