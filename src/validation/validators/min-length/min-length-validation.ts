9615efcbdfaad86071f1e6234e9a2f1c26467607
TiagoDiass <tiago.costadiasss@gmail.com>
2021-02-20 15:20:06 -0300

    refactor: adding string type in MinLengthValidation field param

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -2,7 +2,7 @@ import { InvalidFieldError } from '@/validation/errors';
 import { FieldValidation } from '@/validation/protocols';
 
 export class MinLengthValidation implements FieldValidation {
-  constructor(readonly field, private readonly minLength: number) {}
+  constructor(readonly field: string, private readonly minLength: number) {}
 
   validate(value: string): Error {
     return value.length < this.minLength ? new InvalidFieldError(this.field) : null;
