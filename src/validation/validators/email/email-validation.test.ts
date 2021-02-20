68e2653d19d84d6088dae374b1a7b7c644516181
TiagoDiass <tiago.costadiasss@gmail.com>
2021-02-19 22:17:35 -0300

    refactor: adding a random word on EmailValidation error test

--- src/validation/validators/email/email-validation.test.ts
+++ src/validation/validators/email/email-validation.test.ts
@@ -7,8 +7,9 @@ const makeSut = () => new EmailValidation('email');
 describe('EmailValidation', () => {
   it('should return an error if email is invalid', () => {
     const sut = makeSut();
-    const validationError = sut.validate('invalid_email');
+    const validationError = sut.validate(faker.random.word());
     expect(validationError).toEqual(new InvalidFieldError('E-mail'));
+    expect(validationError.message).toBe('Campo E-mail está com valor inválido');
   });
 
   it('should return falsy if email is valid', () => {
