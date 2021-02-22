2353119bbe65bc6b4c0870664bfe9166f30fe7fc
Tiago Dias <tiago.costadiasss@gmail.com>
2021-02-22 12:46:02 -0300

    feat: ensure ValidationComposite returns an error if any validation fails

--- src/validation/validators/validation-composite/validation-composite.test.ts
+++ src/validation/validators/validation-composite/validation-composite.test.ts
@@ -1,1 +1,16 @@
-export {};
+import { ValidationComposite } from './validation-composite';
+import { FieldValidationSpy } from '@/validation/test';
+
+describe('ValidationComposite', () => {
+  test('should return an error if validation fails', () => {
+    const fieldValidationSpy = new FieldValidationSpy('any_field');
+    const fieldValidationSpy2 = new FieldValidationSpy('any_field');
+    fieldValidationSpy2.error = new Error('any_error_message');
+
+    const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpy2]);
+
+    const validationError = sut.validate('any_field', 'any_value');
+
+    expect(validationError).toBe('any_error_message');
+  });
+});
