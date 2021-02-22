60498a3709ecaec064eb316b9867849274cdca04
Tiago Dias <tiago.costadiasss@gmail.com>
2021-02-22 12:46:26 -0300

    fix: typo in test

--- src/validation/validators/validation-composite/validation-composite.test.ts
+++ src/validation/validators/validation-composite/validation-composite.test.ts
@@ -2,7 +2,7 @@ import { ValidationComposite } from './validation-composite';
 import { FieldValidationSpy } from '@/validation/test';
 
 describe('ValidationComposite', () => {
-  test('should return an error if validation fails', () => {
+  test('should return an error if any validation fails', () => {
     const fieldValidationSpy = new FieldValidationSpy('any_field');
     const fieldValidationSpy2 = new FieldValidationSpy('any_field');
     fieldValidationSpy2.error = new Error('any_error_message');
