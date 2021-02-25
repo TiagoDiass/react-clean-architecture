2925c45e3f45bae11aabfea03f80e1a1f4f615e3
TiagoDiass <tiago.costadiasss@gmail.com>
2021-02-24 22:55:36 -0300

    feat: ensure LocalSaveAccessToken calls setStorage with correct values

---
+++ src/data/usecases/save-access-token/local-save-access-token.test.ts
@@ -0,0 +1,16 @@
+import { LocalSaveAccessToken } from './local-save-access-token';
+import faker from 'faker';
+import { SetStorageSpy } from '@/data/test';
+
+describe('LocalSaveAcessToken', () => {
+  it('should call SetStorage with correct values', async () => {
+    const setStorageSpy = new SetStorageSpy();
+    const sut = new LocalSaveAccessToken(setStorageSpy);
+    const accessToken = faker.random.uuid();
+
+    await sut.save(accessToken);
+
+    expect(setStorageSpy.key).toBe('accessToken');
+    expect(setStorageSpy.value).toBe(accessToken);
+  });
+});
