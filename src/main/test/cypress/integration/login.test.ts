0fd2b0166d6ab51e82c2dd8394296062624d7ae6
TiagoDiass <tiago.costadiasss@gmail.com>
2021-03-14 15:56:01 -0300

    test: ensure Login presents an error state if form is invalid

--- src/main/test/cypress/integration/login.test.ts
+++ src/main/test/cypress/integration/login.test.ts
@@ -1,3 +1,5 @@
+import faker from 'faker';
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -16,5 +18,20 @@ describe('Login', () => {
     cy.getByTestId('error-wrapper').should('not.have.descendants');
     cy.getByTestId('email-input').should('have.value', '');
     cy.getByTestId('password-input').should('have.value', '');
+  });
+
+  it('should present an error state if form is invalid', () => {
+    cy.getByTestId('email-input').type(faker.random.word());
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴');
+
+    cy.getByTestId('password-input').type(faker.random.alphaNumeric(3));
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Valor inválido')
+      .should('contain.text', '🔴');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrapper').should('not.have.descendants');
   });
 });
