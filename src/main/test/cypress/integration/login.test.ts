5f63d98f588faf1852c8edb2b2a4f57c4721207f
TiagoDiass <tiago.costadiasss@gmail.com>
2021-03-15 22:26:49 -0300

    test: ensure Login presents an InvalidCredentialsError on response of status 401(real API)

--- src/main/test/cypress/integration/login.test.ts
+++ src/main/test/cypress/integration/login.test.ts
@@ -2,6 +2,27 @@ import faker from 'faker';
 
 const baseUrl: string = Cypress.config().baseUrl;
 
+function testInvalidCredentialsOnStatus401(intercepRequest: boolean) {
+  if (intercepRequest) {
+    cy.intercept('POST', /login/, {
+      statusCode: 401,
+      body: {
+        error: faker.random.words(),
+      },
+    });
+  }
+
+  cy.getByTestId('email-input').type(faker.internet.email());
+  cy.getByTestId('password-input').type(faker.random.alphaNumeric(5));
+
+  cy.getByTestId('submit').click();
+
+  cy.getByTestId('loading-spinner').should('not.exist');
+  cy.getByTestId('main-error').should('contain.text', 'Credenciais inválidas');
+
+  cy.url().should('equal', `${baseUrl}/login`);
+}
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
@@ -52,87 +73,80 @@ describe('Login', () => {
     cy.getByTestId('error-wrapper').should('not.have.descendants');
   });
 
-  it('should present an InvalidCredentialsError on response of status 401', () => {
-    cy.intercept('POST', /login/, {
-      statusCode: 401,
-      body: {
-        error: faker.random.words(),
-      },
+  describe('using mocked API responses', () => {
+    it('should present an InvalidCredentialsError on response of status 401', () => {
+      testInvalidCredentialsOnStatus401(true);
     });
 
-    cy.getByTestId('email-input').type(faker.internet.email());
-    cy.getByTestId('password-input').type(faker.random.alphaNumeric(5));
+    it('should present an UnexpectedError on response of status 400', () => {
+      cy.intercept('POST', /login/, {
+        statusCode: 400,
+        body: {
+          error: faker.random.words(),
+        },
+      });
 
-    cy.getByTestId('submit').click();
+      cy.getByTestId('email-input').type(faker.internet.email());
+      cy.getByTestId('password-input').type(faker.random.alphaNumeric(5));
 
-    cy.getByTestId('loading-spinner').should('not.exist');
-    cy.getByTestId('main-error').should('contain.text', 'Credenciais inválidas');
+      cy.getByTestId('submit').click();
 
-    cy.url().should('equal', `${baseUrl}/login`);
+      cy.getByTestId('loading-spinner').should('not.exist');
+      cy.getByTestId('main-error').should(
+        'contain.text',
+        'Parece que algo de errado aconteceu. Tente novamente em breve.'
+      );
+
+      cy.url().should('equal', `${baseUrl}/login`);
+    });
+
+    it('should present an UnexpectedError if an invalid property has been returned by the api', () => {
+      cy.intercept('POST', /login/, {
+        statusCode: 200,
+        body: {
+          invalidProperty: faker.random.words(),
+        },
+      });
+
+      cy.getByTestId('email-input').type(faker.internet.email());
+      cy.getByTestId('password-input').type(faker.random.alphaNumeric(5));
+
+      cy.getByTestId('submit').click();
+
+      cy.getByTestId('loading-spinner').should('not.exist');
+      cy.getByTestId('main-error').should(
+        'contain.text',
+        'Parece que algo de errado aconteceu. Tente novamente em breve.'
+      );
+
+      cy.url().should('equal', `${baseUrl}/login`);
+    });
+
+    it('should save accessToken and redirects to home page if valid credentials are provided', () => {
+      cy.intercept('POST', /login/, {
+        statusCode: 200,
+        body: {
+          accessToken: faker.random.uuid(),
+        },
+      });
+
+      cy.getByTestId('email-input').type('mango@gmail.com');
+      cy.getByTestId('password-input').type('12345');
+
+      cy.getByTestId('submit').click();
+
+      cy.getByTestId('main-error').should('not.exist');
+      cy.getByTestId('loading-spinner').should('not.exist');
+
+      cy.url().should('equal', `${baseUrl}/`);
+
+      cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')));
+    });
   });
 
-  it('should present an UnexpectedError on response of status 400', () => {
-    cy.intercept('POST', /login/, {
-      statusCode: 400,
-      body: {
-        error: faker.random.words(),
-      },
+  describe('connecting to the real API', () => {
+    it('should present an InvalidCredentialsError on response of status 401', () => {
+      testInvalidCredentialsOnStatus401(false);
     });
-
-    cy.getByTestId('email-input').type(faker.internet.email());
-    cy.getByTestId('password-input').type(faker.random.alphaNumeric(5));
-
-    cy.getByTestId('submit').click();
-
-    cy.getByTestId('loading-spinner').should('not.exist');
-    cy.getByTestId('main-error').should(
-      'contain.text',
-      'Parece que algo de errado aconteceu. Tente novamente em breve.'
-    );
-
-    cy.url().should('equal', `${baseUrl}/login`);
-  });
-
-  it('should present an UnexpectedError if an invalid property has been returned by the api', () => {
-    cy.intercept('POST', /login/, {
-      statusCode: 200,
-      body: {
-        invalidProperty: faker.random.words(),
-      },
-    });
-
-    cy.getByTestId('email-input').type(faker.internet.email());
-    cy.getByTestId('password-input').type(faker.random.alphaNumeric(5));
-
-    cy.getByTestId('submit').click();
-
-    cy.getByTestId('loading-spinner').should('not.exist');
-    cy.getByTestId('main-error').should(
-      'contain.text',
-      'Parece que algo de errado aconteceu. Tente novamente em breve.'
-    );
-
-    cy.url().should('equal', `${baseUrl}/login`);
-  });
-
-  it('should save accessToken and redirects to home page if valid credentials are provided', () => {
-    cy.intercept('POST', /login/, {
-      statusCode: 200,
-      body: {
-        accessToken: faker.random.uuid(),
-      },
-    });
-
-    cy.getByTestId('email-input').type('mango@gmail.com');
-    cy.getByTestId('password-input').type('12345');
-
-    cy.getByTestId('submit').click();
-
-    cy.getByTestId('main-error').should('not.exist');
-    cy.getByTestId('loading-spinner').should('not.exist');
-
-    cy.url().should('equal', `${baseUrl}/`);
-
-    cy.window().then((window) => assert.isOk(window.localStorage.getItem('accessToken')));
   });
 });
