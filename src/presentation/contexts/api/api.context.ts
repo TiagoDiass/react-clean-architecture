eed87ded5825c7e434bf0c6f479cc73afd00aea4
TiagoDiass <tiago.costadiasss@gmail.com>
2021-03-25 14:34:01 -0300

    feat: add GetCurrentAccount on APIContext types

--- src/presentation/contexts/api/api.context.ts
+++ src/presentation/contexts/api/api.context.ts
@@ -2,7 +2,8 @@ import { AccountModel } from '@/domain/models';
 import { createContext } from 'react';
 
 type APIContextType = {
-  setCurrentAccount: (account: AccountModel) => void;
+  setCurrentAccount?: (account: AccountModel) => void;
+  getCurrentAccount?: () => AccountModel;
 };
 
 export default createContext<APIContextType>(null);
