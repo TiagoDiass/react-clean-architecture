30afd39e4dc09540e8e43b93706bc1b2e0eec3b4
TiagoDiass <tiago.costadiasss@gmail.com>
2021-03-25 14:49:13 -0300

    feat: add get method to LocalStorageAdapter

--- src/infra/cache/local-storage-adapter.test.ts
+++ src/infra/cache/local-storage-adapter.test.ts
@@ -8,7 +8,7 @@ const makeSut = () => new LocalStorageAdapter();
 describe('LocalStorageAdapter', () => {
   beforeEach(localStorage.clear);
 
-  it('should call localStorage with correct values', () => {
+  it('should call localStorage.setItem with correct values', () => {
     const sut = makeSut();
     const key = faker.database.column();
     const value = faker.random.objectElement<AccountModel>();
@@ -16,5 +16,19 @@ describe('LocalStorageAdapter', () => {
     sut.set(key, value);
 
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value));
+  });
+
+  it('should call localStorage.getItem with correct value and receive the correct return', () => {
+    const sut = makeSut();
+    const key = faker.database.column();
+    const value = faker.random.objectElement<AccountModel>();
+
+    const getItemSpy = jest
+      .spyOn(localStorage, 'getItem')
+      .mockReturnValueOnce(JSON.stringify(value));
+
+    const obj = sut.get(key);
+    expect(obj).toEqual(value);
+    expect(getItemSpy).toHaveBeenLastCalledWith(key);
   });
 });
