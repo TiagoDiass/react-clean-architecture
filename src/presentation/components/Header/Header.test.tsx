cdfd1c649bf3ecdf0cbad34d4de959ea22c562c3
TiagoDiass <tiago.costadiasss@gmail.com>
2021-04-03 14:40:14 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/components/Header/Header.test.tsx
+++ src/presentation/components/Header/Header.test.tsx
@@ -6,18 +6,26 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 
+const makeSut = () => {
+  const setCurrentAccoutnMock = jest.fn();
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccoutnMock }}>
+      <Router history={history}>
+        <Header />
+      </Router>
+    </ApiContext.Provider>
+  );
+
+  return {
+    setCurrentAccoutnMock,
+    history,
+  };
+};
+
 describe('Header Component', () => {
   it('should call setCurrentAccount passing null as parameter(Logoff)', () => {
-    const setCurrentAccoutnMock = jest.fn();
-    const history = createMemoryHistory({ initialEntries: ['/'] });
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccoutnMock }}>
-        <Router history={history}>
-          <Header />
-        </Router>
-      </ApiContext.Provider>
-    );
-
+    const { history, setCurrentAccoutnMock } = makeSut();
     fireEvent.click(screen.getByTestId('logout'));
     expect(setCurrentAccoutnMock).toHaveBeenCalledWith(null);
     expect(history.location.pathname).toBe('/login');
